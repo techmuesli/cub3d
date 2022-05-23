5464c939d5992732d36948327ce4e81e0879f306
Daniel Brandtner <dbrandtn@c2s19d128.42wolfsburg.de>
2022-05-23 16:51:55 +0200

    work

--- keys.c
+++ keys.c
@@ -4,33 +4,33 @@
 
 void	key_w(t_data *data)
 {
-	if (data->map.data[(int)data->pos.y][(int)(data->pos.x + data->dir.x * data->move_speed)] == 0)
+	if (data->map.data[data->map.y - (int)data->pos.y][(int)(data->pos.x + data->dir.x * data->move_speed)] == 0)
 		data->pos.x += data->dir.x * data->move_speed;
-	if (data->map.data[(int)(data->pos.y + data->dir.y * data->move_speed)][(int)data->pos.x] == 0)
+	if (data->map.data[data->map.y - (int)(data->pos.y + data->dir.y * data->move_speed)][(int)data->pos.x] == 0)
 		data->pos.y += data->dir.y * data->move_speed;
 }
 
 void	key_a(t_data *data)
 {
-	if (data->map.data[(int)data->pos.y][(int)(data->pos.x + data->camera_plane.x * data->move_speed)] == 0)
+	if (data->map.data[data->map.y - (int)data->pos.y][(int)(data->pos.x + data->camera_plane.x * data->move_speed)] == 0)
 		data->pos.x -= data->camera_plane.x * data->move_speed;
-	if (data->map.data[(int)(data->pos.y + data->camera_plane.y * data->move_speed)][(int)data->pos.x] == 0)
+	if (data->map.data[data->map.y - (int)(data->pos.y + data->camera_plane.y * data->move_speed)][(int)data->pos.x] == 0)
 		data->pos.y -= data->camera_plane.y * data->move_speed;
 }
 
 void	key_d(t_data *data)
 {
-	if (data->map.data[(int)data->pos.y][(int)(data->pos.x + data->camera_plane.x * data->move_speed)] == 0)
+	if (data->map.data[data->map.y - (int)data->pos.y][(int)(data->pos.x + data->camera_plane.x * data->move_speed)] == 0)
 		data->pos.x += data->camera_plane.x * data->move_speed;
-	if (data->map.data[(int)(data->pos.y + data->camera_plane.y * data->move_speed)][(int)data->pos.x] == 0)
+	if (data->map.data[data->map.y - (int)(data->pos.y + data->camera_plane.y * data->move_speed)][(int)data->pos.x] == 0)
 		data->pos.y += data->camera_plane.y * data->move_speed;
 }
 
 void	key_s(t_data *data)
 {
-	if (data->map.data[(int)data->pos.y][(int)(data->pos.x - data->dir.x * data->move_speed)] == 0)
+	if (data->map.data[data->map.y - (int)data->pos.y][(int)(data->pos.x - data->dir.x * data->move_speed)] == 0)
 		data->pos.x -= data->dir.x * data->move_speed;
-	if (data->map.data[(int)(data->pos.y - data->dir.y * data->move_speed)][(int)data->pos.x] == 0)
+	if (data->map.data[data->map.y - (int)(data->pos.y - data->dir.y * data->move_speed)][(int)data->pos.x] == 0)
 		data->pos.y -= data->dir.y * data->move_speed;
 }
 
